68be07b3d0455a44e1714c22950ddca99a3124f4
prashanth-g <prashanthcsgce@gmail.com>
2018-11-16 21:40:45 +0530

    Added config, launch classes

---
+++ src/main/java/com/prashanth/spring/websockets/SpringWebSocketsApplication.java
@@ -0,0 +1,14 @@
+package com.prashanth.spring.websockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebSocketsApplication.class, args);
+    }
+
+}
+
